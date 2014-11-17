e336e62db8e1da1495f6047b946ec3c689f3d9f6
baltena <bart.altena@hotmail.com>
2014-11-17 15:28:28 +0100

    Dynamic loading custom config
    With this config file packer-unit will recognize if the user created a custom config and load it. If there is no custom config it will use a default config. 
    
    (Also removed the reporting functionality, this is optional and can be done in the custom config)

--- grunt_config/karma.js
+++ grunt_config/karma.js
@@ -1,32 +1,41 @@
-module.exports = {
-	karma: {
-		options: {
-			basePath: '',
-   			frameworks: ['jasmine'],
-      		configFile: 'test/karma.config.js',
-      		browsers:[
-      			'Chrome'
-      		],
-      		reporters: ['progress', 'coverage'],
-		    preprocessors: {
-		      'src/app/*.js': ['coverage'],
-		      'src/app/*/*.js': ['coverage']
-		    },
-		    coverageReporter: {
-		      type : 'html',
-		      dir : './coverage/'
-		    }
-    	},
-		unit: {
-			options: {
-	      		singleRun : true
-	    	}
-	    },
-	    watch: {
-			options: {
-	      		singleRun : false,
-	      		autoWatch: true
-	    	}
-	    }
-	}
+var grunt = require('grunt');
+
+var karmaconfig;
+
+if (grunt.file.exists('test/karma.config.js')) {
+    karmaconfig = require('../../../test/karma.config.js')
 }
+
+else {
+    karmaconfig = {
+        karma: {
+            options: {
+                basePath: '',
+                frameworks: ['jasmine'],
+                files: [
+                    'bower_components/angular/angular.js',
+                    'bower_components/angular-*/angular-*.js',
+                    'src/app/*.js',
+                    'src/app/**/*.js',
+                    'test/unit/**/*.js'
+                ],
+                browsers: [
+                    'Chrome'
+                ]
+            },
+            unit: {
+                options: {
+                    singleRun: true
+                }
+            },
+            watch: {
+                options: {
+                    singleRun: false,
+                    autoWatch: true
+                }
+            }
+        }
+    }
+}
+
+module.exports = karmaconfig;
